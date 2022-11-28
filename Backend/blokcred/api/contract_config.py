d2e99f84c4413b94bf077035b7425006e53869aa
Navraj <navraj2071@gmail.com>
2022-11-28 22:12:59 +0530

    removed unneccessary files

--- Backend/blokcred/api/contract_config.py
+++
@@ -1,5 +0,0 @@
-config = {
-    "contract_address": "0x2d8353ffbEC05e7D4520100Cdb413Ee912f23797",
-    "public_key": "0xEB55FAbfF3dC5Fd042f9dED49b0fff9063cc8A8d",
-    "private_key": "5554aa27c5b045a5bf758779b248f0691246d564feef0f3b10fe87767a2c9d60",
-}
